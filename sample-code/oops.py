83871eb5a25bb81e793a01b59ffe2354e2c33ca3
Abhi <guptaabhinav609@gmail.com>
2026-06-11 18:01:43 +0530

    Create oops.py
    classes and objects in python

---
+++ sample-code/oops.py
@@ -0,0 +1,32 @@
+"""
+class Factory:
+    a = 12 #attribute
+
+    def hello(self): #method
+        print("how are you?")
+
+    print("i am getting initialized")
+
+print(Factory().a)
+Factory().hello()
+
+obj = Factory()
+print(obj.a)
+obj.hello()
+"""
+class Factory:
+
+    def __init__(self, material, zips, pockets):
+        self.material = material
+        self.zips = zips
+        self.pockets = pockets
+
+    def show(self):
+        print(f"object details are {self.material}, {self.zips}, {self.pockets}")
+
+reebok = Factory("leather", 3, 2)
+
+campus = Factory("nylon", 3, 3)
+
+print(campus.pockets)
+print(campus.show())
