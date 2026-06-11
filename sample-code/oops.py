ad65e11cd38ca91a510b6dfa01270887746e47a6
Abhi <guptaabhinav609@gmail.com>
2026-06-11 18:40:26 +0530

    Update oops.py
    inheritance

--- sample-code/oops.py
+++ sample-code/oops.py
@@ -30,13 +30,39 @@ campus = Factory("nylon", 3, 3)
 
 print(campus.pockets)
 print(campus.show())
-"""
+
 class Animal:
+    
     name = "lion" #class attribute
     
-    def __init(self, age):
+    def __init__(self, age):
         self.age = age #instance attribute
 
     def instance_method(self): #instance method
-        print("instance method")
-        
+        print(f"instance method your age is {self.age}")
+
+    @classmethod
+    def hello(cls): #class method
+        print("inside class method")
+
+    @staticmethod
+    def static_method(): #static method
+        print("inside static method")
+
+obj = Animal(12)
+obj.instance_method()
+obj.hello()
+obj.static_method()
+"""
+class Parent: #parent class / super class
+    a = 12
+    def show(self):
+        print("inside the show method")
+
+class Child(Parent): #child class / sub class
+    pass
+
+obj = Parent()
+print(obj.a)
+obj1 = Child()
+obj1.show()
